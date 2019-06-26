35a35054ff653bdf91d7bb4dc702b8431522662b
Hugo Oliveira <oliveirahugo.97@gmail.com>
2019-06-26 17:37:49 +0100

    fix api requests by using user token

--- dsm-frontend/src/services/api/index.js
+++ dsm-frontend/src/services/api/index.js
@@ -1,46 +1,35 @@
 import axios from 'axios';
 import apiConfig from './config';
 
-//axios.defaults.withCredentials = true;
 
 export const fetchApi = (
     method, endPoint,
     payload = {}, headers = {},
     successHandler, errorHandler) => {
 
+
+    // by default
+    let authorization = apiConfig.accessToken;
+
+    //check if login authentication
+    const userToken = localStorage.getItem('userToken');
+    const userType = localStorage.getItem('userType');
+
+    if(userToken && userType)
+        authorization = userToken;
+
     axios({
         url: apiConfig.url + endPoint,
         method: method,
         data: payload,
         headers: {
-            //crossDomain: true,
             'Content-Type': 'application/json',
-            'Authorization':  apiConfig.accessToken,
+            'Authorization':  authorization,
             ...headers
         },
-        //withCredentials: true,
     }).then(response => {
         successHandler(response);
     }).catch(error => {
         errorHandler(error);
     });
-
-    /*
-    fetch(apiConfig.url + endPoint, {
-        method: method,
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization':  apiConfig.accessToken,
-            ...headers
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => {
-            successHandler(json);
-            console.log(json);
-        }).catch((err) => {
-            console.log("ERROR: " + err);
-    });
-     */
 };
